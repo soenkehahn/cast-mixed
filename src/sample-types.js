c47d315681256ba67143a27aa5ce5ef2be3788eb
Sönke Hahn <soenkehahn@gmail.com>
2017-04-16 02:32:37 -0400

    bigger refactoring and unions

--- src/sample-types.js
+++ src/sample-types.js
@@ -2,8 +2,24 @@
 
 import _ from 'lodash'
 
-export function cast<A>(sample: A, input: mixed): ?A {
-  if (hasSampleType(sample, input)) {
+export class Spec<Sample> {
+
+  sample: Sample
+
+  conformsToSample: mixed => bool
+
+  constructor(sample: Sample, conformsToSample: mixed => bool) {
+    this.sample = sample,
+    this.conformsToSample = conformsToSample
+  }
+}
+
+function getSample<Sample>(spec: Spec<Sample>): Sample {
+  return spec.sample
+}
+
+export function cast<A>(spec: Spec<A>, input: mixed): ?A {
+  if (spec.conformsToSample(input)) {
     return unsafeCast(input)
   } else {
     return null
@@ -15,65 +31,72 @@ function unsafeCast<A, B>(a: A): B {
   return r
 }
 
-function hasSampleType<A>(sample: A, input: mixed): bool {
-  switch (typeof(sample)) {
-    case 'number':
-      if (typeof(input) === 'number') {
-        return true
-      } else {
-        return false
-      }
-    case 'string':
-      if (typeof(input) === 'string') {
-        return true
-      } else {
-        return false
-      }
-    case 'object':
-      if (sample === null) {
-        throw new Error('invalid sample: null')
-      } else if (Array.isArray(sample)) {
-        return _arrayHasSampleType(sample, input)
-      } else if (typeof(input) === 'object' && input !== null) {
-        return _objectHasSampleType(sample, input)
-      } else {
-        return false
-      }
-    case 'undefined':
-      throw new Error('invalid sample: undefined')
-    default:
-      const s: any = sample
-      throw new Error('invalid sample: ' + s.toString())
-  }
-}
+// * spec creation functions
 
-function _objectHasSampleType<A>(sample: {} & A, input: {}): bool {
-  let canCast = true
-  _.forEach(sample, (value, key) => {
-    if (input.hasOwnProperty(key)) {
-      let castField = cast(value, input[key])
-      if (castField === null) {
-        canCast = false
+export const number: Spec<number> = new Spec(42, mixed => {
+  return (typeof(mixed) === 'number')
+})
+
+export const string: Spec<string> = new Spec('foo', mixed => {
+  return (typeof(mixed) === 'string')
+})
+
+export const boolean: Spec<boolean> = new Spec(true, mixed => {
+  return (typeof(mixed) === 'boolean')
+})
+
+export function object<A, O: { [string]: Spec<A>}>(object: O): Spec<$ObjMap<O, typeof(getSample)>> {
+  const result = {}
+  Object.keys(object).forEach(key => {
+    result[key] = getSample(object[key])
+  })
+  return new Spec(result, mixed => {
+    if (typeof(mixed) === 'object') {
+      if (mixed === null) {
+        return false
+      } else {
+        let canCast = true
+        const obj: {} = mixed
+        _.forEach(object, (valueSpec, key) => {
+          if (obj.hasOwnProperty(key)) {
+            canCast = canCast && valueSpec.conformsToSample(obj[key])
+          } else {
+            canCast = false
+          }
+        })
+        return canCast
       }
     } else {
-      canCast = false
+      return false
     }
   })
-  return canCast
 }
 
-function _arrayHasSampleType<E>(sample: Array<E>, input: mixed): bool {
-  if (sample.length !== 1) {
-    throw new Error('invalid sample: array sample must have exactly 1 argument: ' + JSON.stringify(sample))
-  }
-  const elementSample = sample[0]
-  if (! Array.isArray(input)) {
-    return false
-  } else {
-    let canCast = true
-    input.forEach(element => {
-      canCast = canCast && hasSampleType(elementSample, element)
-    })
-    return canCast
-  }
+export function array<A>(elementSpec: Spec<A>): Spec<Array<A>> {
+  return new Spec([elementSpec.sample], mixed => {
+    if (Array.isArray(mixed)) {
+      let canCast = true
+      mixed.forEach(element => {
+        canCast = canCast && elementSpec.conformsToSample(element)
+      })
+      return canCast
+    } else {
+      return false
+    }
+  })
+}
+
+export function union<A, B>(a: Spec<A>, b: Spec<B>): Spec<A | B> {
+  return new Spec(a.sample, mixed => {
+    return a.conformsToSample(mixed)
+      || b.conformsToSample(mixed)
+  })
+}
+
+export function union3<A, B, C>(a: Spec<A>, b: Spec<B>, c: Spec<C>): Spec<A | B | C> {
+  return new Spec(a.sample, mixed => {
+    return a.conformsToSample(mixed)
+      || b.conformsToSample(mixed)
+      || c.conformsToSample(mixed)
+  })
 }
--- tests/sample-types_test.js
+++ tests/sample-types_test.js
@@ -3,44 +3,24 @@
 import {describe, it} from 'mocha'
 import {expect} from 'chai'
 
-import {cast} from '../src/sample-types'
+import {Spec, cast, number, string, boolean, object, array, union, union3} from '../src/sample-types'
 
-function test<A>(sample: A, input: mixed, expected: ?A) {
+function test<A>(sample: Spec<A>, input: mixed, expected: ?A) {
   const result = cast(sample, input)
   expect(result).to.eql(expected)
 }
 
-
 describe('sample-types', () => {
   describe('cast', () => {
-
-    describe('invalid samples', () => {
-
-      it('throws an exception in case of invalid samples', () => {
-        const f = () => cast(() => {}, 5)
-        expect(f).to.throw(/^invalid sample: \(\) => {}$/)
-      })
-
-      it("doesn't allow null as samples", () => {
-        const f = () => cast(null, null)
-        expect(f).to.throw(/^invalid sample: null$/)
-      })
-
-      it("doesn't allow undefined as samples", () => {
-        const f = () => cast(undefined, undefined)
-        expect(f).to.throw(/^invalid sample: undefined$/)
-      })
-
-    })
 
     describe('numbers', () => {
 
       it('allows to cast', () => {
-        test(42, 5, 5)
+        test(number, 5, 5)
       })
 
       it('rejects other types', () => {
-        test(42, {foo: 5}, null)
+        test(number, {foo: 5}, null)
       })
 
     })
@@ -48,42 +28,54 @@ describe('sample-types', () => {
     describe('strings', () => {
 
       it('allows to cast', () => {
-        test('foo', 'bar', 'bar')
+        test(string, 'bar', 'bar')
       })
 
       it('rejects other types', () => {
-        test('foo', 5, null)
+        test(string, 5, null)
+      })
+
+    })
+
+    describe('booleans', () => {
+
+      it('allows to cast', () => {
+        test(boolean, true, true)
+      })
+
+      it('rejects other types', () => {
+        test(boolean, 5, null)
       })
 
     })
 
     describe('objects', () => {
       it('allows to cast', () => {
-        test({foo: 42}, {foo: 5}, {foo: 5})
+        test(object({foo: number}), {foo: 5}, {foo: 5})
       })
 
       it('rejects other types', () => {
-        test({foo: 42}, 'foo', null)
+        test(object({foo: number}), 'foo', null)
       })
 
       describe('recursively checks fields', () => {
 
         it('allows to cast field values', () => {
-          test({foo: {bar: 42}}, {foo: {bar: 5}}, {foo: {bar: 5}})
+          test(object({foo: object({bar: number})}), {foo: {bar: 5}}, {foo: {bar: 5}})
         })
 
         it('rejects other types in field values', () => {
-          test({foo: {bar: 42}}, {foo: {bar: 'foo'}}, null)
+          test(object({foo: object({bar: number})}), {foo: {bar: 'foo'}}, null)
         })
 
         it('rejects objects with missing fields', () => {
-          test({foo: 42, bar: 42}, {foo: 5}, null)
+          test(object({foo: number, bar: number}), {foo: 5}, null)
         })
 
       })
 
       it('allows more fields than in the sample', () => {
-        test({foo: 42}, {foo: 5, bar: 6}, {foo: 5, bar: 6})
+        test(object({foo: number}), {foo: 5, bar: 6}, {foo: 5, bar: 6})
       })
 
       it('allows optional fields')
@@ -100,40 +92,43 @@ describe('sample-types', () => {
     describe('arrays', () => {
 
       it('allows to cast to arrays', () => {
-        test(['foo'], ['bar'], ['bar'])
+        test(array(string), ['bar'], ['bar'])
       })
 
       it('rejects other types', () => {
-        test(['foo'], {'foo': 'bar'}, null)
+        test(array(string), {'foo': 'bar'}, null)
       })
 
       it('returns null in case one element is off', () => {
-        test(['foo'], ['foo', 4, 'bar'], null)
-      })
-
-      describe('invalid array samples', () => {
-
-        it('disallows sample-arrays with no elements', () => {
-          const f = () => cast([], [])
-          expect(f).to.throw(/^invalid sample: array sample must have exactly 1 argument: \[\]$/)
-        })
-
-        it('disallows sample-arrays with more than one element', () => {
-          const f = () => cast(['foo', 'bar'], ['foo'])
-          expect(f).to.throw(/^invalid sample: array sample must have exactly 1 argument: \["foo","bar"\]$/)
-        })
-
+        test(array(string), ['foo', 4, 'bar'], null)
       })
 
     })
 
     describe('unions', () => {
 
-      it('allows to specify unions')
+      it('allows to cast to the first type', () => {
+        test(union(number, string), 5, 5)
+      })
+
+      it('allows to cast to the second type', () => {
+        test(union(number, string), 'foo', 'foo')
+      })
+
+      it('rejects other types', () => {
+        test(union(number, string), {foo: 5}, null)
+      })
+
+      it('works for three types', () => {
+        test(union3(number, string, array(number)), 5, 5)
+        test(union3(number, string, array(number)), 'foo', 'foo')
+        test(union3(number, string, array(number)), [1, 2, 3], [1, 2, 3])
+      })
 
     })
 
     it('allows nullable types')
 
   })
+
 })
